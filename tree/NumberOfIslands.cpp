794fd920fed5672e9aef8f195ac96ab387957a53
Karan Nandkar <knandkar007@gmail.com>
2026-06-03 21:27:33 +0530

    Add NumberOfIslands solution and CMake entry
    Add a new DFS-based solution for the Number of Islands problem at tree/NumberOfIslands.cpp. Implements numIslands(vector<vector<char>>&) with a recursive dfs helper that marks visited land in-place by flipping '1' to '0'. Also update CMakeLists.txt to include the new source file in the dsa_pratise executable.

---
+++ tree/NumberOfIslands.cpp
@@ -0,0 +1,65 @@
+﻿//
+// Created by Karan Nandkar on 03-06-2026.
+//
+
+/**
+ * Given:
+ * grid =
+ * [
+ *   ['1','1','0','0'],
+ *   ['1','0','0','1'],
+ *   ['0','0','1','1']
+ * ]
+ *
+ * Return number of islands
+ *
+ * An island is connected land '1' horizontally or vertically
+ */
+
+
+#include <vector>
+using namespace std;
+
+void dfs(vector<vector<char>>& grid, int r, int c)
+{
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    if (r < 0 || r >= rows || c < 0 || c >= cols)
+        return;
+
+    if (grid[r][c] != '1')
+        return;
+
+    grid[r][c] = '0';
+
+    dfs(grid, r + 1, c);
+    dfs(grid, r - 1, c);
+    dfs(grid, r, c + 1);
+    dfs(grid, r, c - 1);
+}
+
+int numIslands(vector<vector<char>>& grid)
+{
+    if (grid.empty())
+        return 0;
+
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    int islandCount = 0;
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            if (grid[r][c] == '1')
+            {
+                islandCount++;
+                dfs(grid, r, c);
+            }
+        }
+    }
+
+    return islandCount;
+}
