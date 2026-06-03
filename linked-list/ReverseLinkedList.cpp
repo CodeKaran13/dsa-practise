d8c5482d9c16f888c07acbeb667faee41cd49fe4
Karan Nandkar <knandkar007@gmail.com>
2026-06-03 22:51:35 +0530

    Add reverse linked list and CMake entry
    Introduce linked-list/ReverseLinkedList.cpp implementing an iterative reverseList function and ListNode struct (O(n) time, O(1) space). Update CMakeLists.txt to include the new source so it's built with the project.

---
+++ linked-list/ReverseLinkedList.cpp
@@ -0,0 +1,27 @@
+﻿//
+// Created by Karan Nandkar on 03-06-2026.
+//
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head)
+{
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+
+    while (curr)
+    {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
