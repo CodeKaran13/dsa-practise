da4899ff7004f94d88d5b58c5fe1bf3afb07c992
Karan Nandkar <knandkar007@gmail.com>
2026-06-03 21:00:31 +0530

    Add TopKFrequentElements.cpp and CMake entry
    Implement topKFrequent using an unordered_map to count frequencies and a min-heap (priority_queue of (count, num)) that maintains the top k elements by frequency. The new function topKFrequent(const vector<int>&, int) returns the collected elements. Also update CMakeLists.txt to include heap/TopKFrequentElements.cpp in the dsa_pratise target.

---
+++ heap/TopKFrequentElements.cpp
@@ -0,0 +1,43 @@
+﻿//
+// Created by Karan Nandkar on 03-06-2026.
+//
+
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> topKFrequent(const vector<int>& nums, int k)
+{
+    unordered_map<int, int> freq;
+    for (int num : nums)
+    {
+        freq[num]++;
+    }
+
+    using Pair = pair<int, int>;
+    priority_queue<Pair, vector<Pair>, greater<Pair>> minHeap;
+
+    for (const auto& pair : freq)
+    {
+        int num = pair.first;
+        int count = pair.second;
+
+        minHeap.push({count, num});
+
+        if (minHeap.size() > k)
+        {
+            minHeap.pop();
+        }
+    }
+
+    vector<int> result;
+    while (!minHeap.empty())
+    {
+        result.push_back(minHeap.top().second);
+        minHeap.pop();
+    }
+
+    return result;
+}
