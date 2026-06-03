9ca2b1bb7774abf273a2672f59db0524a6f63133
Karan Nandkar <knandkar007@gmail.com>
2026-06-03 20:45:21 +0530

    Add KthLargest heap solution and CMake
    Introduce heap/KthLargest.cpp implementing findKthLargest using a min-heap (priority_queue with greater<int>) to maintain the k largest elements, yielding O(n log k) time and O(k) space. Update CMakeLists.txt to include the new source file in the dsa_pratise executable.

---
+++ heap/KthLargest.cpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by Karan Nandkar on 03-06-2026.
+//
+
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int findKthLargest(const vector<int>& nums, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for (int num : nums)
+    {
+        minHeap.push(num);
+        if (minHeap.size() > k)
+        {
+            minHeap.pop();
+        }
+    }
+
+    return minHeap.top();
+}
