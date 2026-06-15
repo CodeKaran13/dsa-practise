c8fdcfab38b0af109355d71993fe82e89bf91319
Karan Nandkar <knandkar007@gmail.com>
2026-06-15 08:37:26 +0530

    Update ContainsDuplicate.cpp

--- Array-Hashing/ContainsDuplicate.cpp
+++ Array-Hashing/ContainsDuplicate.cpp
@@ -2,28 +2,20 @@
 // Created by Karan Nandkar on 14/05/26.
 //
 
-/*
- * Contains Duplicate
- * Given: [1,2,3,1]
- *
- * Return: true
- *
- */
-
 #include <unordered_set>
 #include <vector>
 
-bool containsDuplicate(const std::vector<int>& nums)
+bool ContainsDuplicate(const std::vector<int>& nums)
 {
     std::unordered_set<int> seen;
     for (int num : nums)
     {
         if (seen.contains(num))
-        {
             return true;
-        }
+
         seen.insert(num);
     }
+
     return false;
 }
 
@@ -31,11 +23,8 @@ bool containsDuplicate(const std::vector<int>& nums)
 bool containsDuplicateSorted(const std::vector<int>& nums)
 {
     for (int i = 1; i < nums.size(); i++)
-    {
         if (nums[i] == nums[i - 1])
-        {
             return true;
-        }
-    }
+
     return false;
 }
